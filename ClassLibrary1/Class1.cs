baed9640d2f70a11d5407238433be81fc61c7b61
Anne Damsgaard <andamsgaard@gmail.com>
2020-02-05 15:29:02 +0100

    Test projekt ændret til fremwork

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_Uge5;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Calculator calculator;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TestCase(2, 4, 6)]
+        [TestCase(-2, -4, -6)]
+
+
+        public void Add(double a, double b, double x)
+        {
+            Assert.That(calculator.Add(a, b), Is.EqualTo(x));
+        }
+
+
+        [TestCase(2, 4, -2)]
+        [TestCase(-2, -4, 2)]
+
+
+        public void Subtract(double a, double b, double x)
+        {
+            Assert.That(calculator.Subtract(a, b), Is.EqualTo(x));
+        }
+
+
+        [TestCase(2, 5, 10)]
+        [TestCase(5, 7, 35)]
+        public void Multiply(double a, double b, double res)
+        {
+            Assert.That(calculator.Multiply(a, b), Is.EqualTo(res));
+
+        }
+
+        [Test]
+        public void Power_4opi2_Return16()
+        {
+            Assert.That(calculator.Power(2, 4), Is.EqualTo(16));
+        }
+
+
+    }
+}
